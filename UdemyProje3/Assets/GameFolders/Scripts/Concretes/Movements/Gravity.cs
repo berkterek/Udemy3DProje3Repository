4de1c6c0fa24a90e19630ce25a40bf53e4ddbc03
berkterek <42406263+berkterek@users.noreply.github.com>
2021-03-28 11:17:41 +0300

    Game Ui Design
    Game Ui Design yapildi

---
+++ UdemyProje3/Assets/GameFolders/Scripts/Concretes/Movements/Gravity.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyProject3.Movements
+{
+    public class Gravity : MonoBehaviour
+    {
+        [SerializeField] float _gravity = -9.81f;
+
+        CharacterController _characterController;
+        Vector3 _velocity;
+
+        void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        void Update()
+        {
+            if (_characterController.isGrounded) _velocity.y = 0f;
+
+            _velocity.y += _gravity * Time.deltaTime;
+
+            _characterController.Move(_velocity * Time.deltaTime);
+        }
+    }
+}
