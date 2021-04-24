61a3d54735d9cf80b7be0f8847d099fd77a39555
berkterek <42406263+berkterek@users.noreply.github.com>
2021-04-24 13:59:54 +0300

    Created State Machine
    State Machine olsuturuldu

--- UdemyProje3/Assets/GameFolders/Scripts/Abstracts/States/IState.cs
+++ UdemyProje3/Assets/GameFolders/Scripts/Abstracts/States/IState.cs
@@ -4,5 +4,8 @@ namespace UdemyProject3.Abstracts.States
     public interface IState
     {
         void Tick();
+        void OnExit();
+        void OnEnter();
+        
     }    
 }
--- UdemyProje3/Assets/GameFolders/Scripts/Concretes/States/StateMachine.cs
+++ UdemyProje3/Assets/GameFolders/Scripts/Concretes/States/StateMachine.cs
@@ -1,18 +1,61 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UdemyProject3.Abstracts.States;
+using UdemyProject3.States;
 
-public class StateMachine : MonoBehaviour
+public class StateMachine
 {
-    // Start is called before the first frame update
-    void Start()
+    List<StateTransformer> _stateTransformers = new List<StateTransformer>();
+    List<StateTransformer> _anyStateTransformer = new List<StateTransformer>();
+
+    IState _currentState;
+
+    public void SetState(IState state)
     {
-        
+        if (_currentState == state) return;
+
+        _currentState?.OnExit();
+
+        _currentState = state;
+
+        _currentState.OnEnter();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Tick()
     {
+        StateTransformer stateTransformer = CheckForTransformer();
+
+        if (stateTransformer != null)
+        {
+            SetState(stateTransformer.To);
+        }
         
+        _currentState.Tick();
+    }
+
+    private StateTransformer CheckForTransformer()
+    {
+        foreach (StateTransformer stateTransformer in _anyStateTransformer)
+        {
+            if (stateTransformer.Condition.Invoke()) return stateTransformer;
+        }
+
+        foreach (StateTransformer stateTransformer in _stateTransformers)
+        {
+            if (stateTransformer.Condition.Invoke() && _currentState == stateTransformer.From) return stateTransformer;
+        }
+
+        return null;
+    }
+
+    public void AddState(IState from, IState to, System.Func<bool> condition)
+    {
+        StateTransformer stateTransformer = new StateTransformer(from, to, condition);
+        _stateTransformers.Add(stateTransformer);
+    }
+
+    public void AddAnyState(IState to, System.Func<bool> condition)
+    {
+        StateTransformer stateTransformer = new StateTransformer(null, to, condition);
+        _anyStateTransformer.Add(stateTransformer);
     }
 }
